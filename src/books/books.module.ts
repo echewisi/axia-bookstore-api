5b27a2d4a56c26702339a1cb2df4c8c229c082de
Joseph Echewisi <josephechewisi@gmail.com>
2024-10-30 19:36:11 +0100

    ...

---
+++ src/books/books.module.ts
@@ -0,0 +1,20 @@
+// books/books.module.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
+import { Book, BookSchema } from './schema/books.schema';
+import { AuthorsModule } from '../authors/author.module';
+import { GenresModule } from '../genres/genre.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]), // Register Book schema
+    AuthorsModule,  
+    GenresModule,   
+  ],
+  controllers: [BooksController],
+  providers: [BooksService],
+  exports: [BooksService],
+})
+export class BooksModule {}
